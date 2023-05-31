55d63e0a840d248338fa5cfb1e6f1cf3d8037a26
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2023-05-31 14:06:36 +0400

    add home fragment and viewModel

---
+++ app/src/main/java/com/space/quizapp/ui/home/HomeFragment.kt
@@ -0,0 +1,17 @@
+package com.space.quizapp.ui.home
+
+import com.space.quizapp.databinding.FragmentHomeBinding
+import com.space.quizapp.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    override fun onBind() {
+    }
+
+    override fun observes() {
+    }
+
+    override fun listeners() {
+
+    }
+}
---
+++ app/src/main/java/com/space/quizapp/ui/home/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package com.space.quizapp.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+}
