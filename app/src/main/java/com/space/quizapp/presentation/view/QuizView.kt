1133d030ec34b7549903a774657b665ad35be816
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2023-06-09 16:17:43 +0400

    add float to int fun

--- app/src/main/java/com/space/quizapp/presentation/view/QuizView.kt
+++ app/src/main/java/com/space/quizapp/presentation/view/QuizView.kt
@@ -20,12 +20,25 @@ class QuizView(
             iconImage.loadImage(point.quizIcon)
             titleText.text = point.quizTitle
             descriptionText.text = point.quizDescription
-            if (point.point.toString().toIntOrNull() != null)
-                pointText.text = point.point.toInt().toString()
-            else
-                pointText.text = point.point.toString()
+
+            pointText.text = floatToPoint(point.point)
 
             nextImage.visibility = View.GONE
         }
+    }
+
+    private fun floatToPoint(float: Float): String {
+
+        // Convert float value
+        // of N to integer
+        val x = float.toInt()
+        val temp2 = float - x
+
+        // If N is not equivalent
+        // to any integer
+        return if (temp2 > 0)
+            float.toString()
+        else
+            float.toInt().toString()
     }
 }
